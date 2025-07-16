0f7d27f8efa01e6a08652f5bb94ebbe5e6e0ca08
rohitghatal1 <rohitghatal@gmail.com>
2025-07-16 16:20:30 +0545

    added database configuration

---
+++ config/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI!)
+        console.log("Mongo DB connected")
+    } catch (err){
+        console.error("DB Connection failed:", err)
+        process.exit(1);
+    }
+}
