bce2cdcbdd1992b0aff4e85ddd48e3b3a1315cab
rohitghatal1 <rohitghatal@gmail.com>
2025-07-17 23:45:15 +0545

    updated

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+export const authenticate = (req: any, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
+
   if (!token) return res.sendStatus(401);
+  
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { id: string };
     req.userId = decoded.id;
--- models/UserModel.ts
+++ models/UserModel.ts
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: String,
+  number: String,
+  address: String,
   email: { type: String, unique: true },
   password: String,
+
 });
 
 export const User = mongoose.model("User", userSchema);
