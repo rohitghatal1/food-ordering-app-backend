9288028b60e500d3abd57f894c7fe2a969292150
rohitghatal1 <rohitghatal@gmail.com>
2025-07-19 21:57:35 +0545

    updated

--- controllers/foodController.ts
+++ controllers/foodController.ts
@@ -1,12 +1,11 @@
-import { Request, Response } from "express";
 import { Food } from "../models/FoodModel";
 
-export const getFoods = async (_: Request, res: Response) => {
+export const getFoods = async (_: any, res: any) => {
   const foods = await Food.find();
   res.json(foods);
 };
 
-export const addFood = async (req: Request, res: Response) => {
+export const addFood = async (req: any, res: any) => {
   const food = new Food(req.body);
   await food.save();
   res.status(201).json(food);
