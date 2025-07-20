29cee4b4351309910776c3638aa679e4f571ba62
rohitghatal1 <rohitghatal@gmail.com>
2025-07-20 14:00:00 +0545

    updated

--- index.ts
+++ index.ts
@@ -2,3 +2,11 @@ import mongoose from "mongoose";
 import express from "express"
 import dotenv from "dotenv"
 
+import cors from "cors";
+
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+
