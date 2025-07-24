f2a4436261f350168891a73830b420b43a0a2e84
rohitghatal1 <rohitghatal@gmail.com>
2025-07-24 22:19:47 +0545

    updated index file

--- index.ts
+++ index.ts
@@ -1,18 +1,47 @@
-import express from "express"
-import dotenv from "dotenv"
 import errorHandler from "./utils/errorHandler";
+const express = require("express");
+const mongoose = require("mongoose");
+import dotenv from "dotenv";
+const cors = require("cors");
+const http = require("http");
+const socketio = require("socket.io");
+
 import authRoutes from "./routes/authRouth";
+import orderRoutes from "./routes/orderRoutes";
 import foodRoutes from "./routes/foodRoutes";
-import orderRoutes from "./routes/orderRoutes"
+
+
 
 dotenv.config();
+const app = express();
+const server = http.createServer(app);
 
-export const app = express();
-
+//middleware
+app.use(cors());
 app.use(express.json());
 
+
+//Routes
+app.get("/", (req: any, res: any) => {
+  res.send("QuickChat backend is running!");
+});
+
+// user routes
 app.use("/api/auth", authRoutes);
-app.use("/api/foods", foodRoutes);
-app.use("/api/orders", orderRoutes);
+app.use("/api/chat", foodRoutes);
+app.use("/api/users/", orderRoutes);
+
+
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCh", "DELETE"],
+  },
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+});
 
 app.use(errorHandler);
