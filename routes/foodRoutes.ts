297fc04646f6a4c602ab605aa4777033158af330
rohitghatal1 <rohitghatal@gmail.com>
2025-08-06 23:13:47 +0545

    updated

--- routes/foodRoutes.ts
+++ routes/foodRoutes.ts
@@ -1,10 +1,17 @@
 import express from "express"
+import { getFoods } from "../controllers/foodController";
 const {Router} = express;
 
 const router = Router();
 
 router.post("/food", async (req:any, res:any) =>{
-    
+
 })
+
+router.delete("/food", async (req:any, res:any) => {
+
+})
+
+router.get("/food", getFoods);
 
 export default router;
